7f1b3756ad2fec604c2e7dabf27627b0af93b18b
SessevmezBugra <sessevmezbugra@gmail.com>
2019-02-09 13:38:31 +0300

    last 3333

---
+++ PetClinicApp/src/main/java/com/javaegitimleri/petclinic/service/PetClinicServiceImpl.java
@@ -0,0 +1,83 @@
+package com.javaegitimleri.petclinic.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.javaegitimleri.petclinic.dao.OwnerRepository;
+import com.javaegitimleri.petclinic.dao.PetRepository;
+import com.javaegitimleri.petclinic.exception.OwnerNotFoundException;
+import com.javaegitimleri.petclinic.model.Owner;
+
+@Service
+@Transactional(rollbackFor=Exception.class)
+public class PetClinicServiceImpl implements PetClinicService {
+	
+	private OwnerRepository ownerRepository;
+	
+	private PetRepository petRepository; 
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	@Autowired
+	public void setOwnerRepositoryitory(OwnerRepository ownerRepository) {
+		this.ownerRepository = ownerRepository;
+	}
+	
+	@Autowired
+	public void setPetRepository(PetRepository petRepository) {
+		this.petRepository = petRepository;
+	}
+	
+	@Override
+	@Transactional(propagation=Propagation.SUPPORTS,readOnly=true)
+//	@Secured(value= {"ROLE_USER","ROLE_EDITOR"})
+	public List<Owner> findOwners() {
+		return ownerRepository.findAll();
+	}
+
+	@Override
+	@Transactional(propagation=Propagation.SUPPORTS,readOnly=true)
+	public List<Owner> findOwners(String lastName) {
+		return ownerRepository.findByLastName(lastName);
+	}
+
+	@Override
+	public Owner findOwner(Long id) throws OwnerNotFoundException {
+		Owner owner = ownerRepository.findById(id);
+		if(owner == null) throw new OwnerNotFoundException("Owner not found with id : "+id);
+		return owner;
+	}
+
+	@Override
+	public void createOwner(Owner owner) {
+		ownerRepository.create(owner);
+		
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setFrom("sessevmezbugra@gmail.com");
+		mailMessage.setTo("bugra.sessevmez@visteknoloji.com");
+		mailMessage.setSubject("Owner created!");
+		mailMessage.setText("Owner entity with id :"+owner.getId()+" created successfully.");
+		mailSender.send(mailMessage);
+	}
+
+	@Override
+	public void update(Owner owner) {
+		ownerRepository.update(owner);
+	}
+
+	@Override
+	public void deleteOwner(Long id) {
+		petRepository.deleteByOwnerId(id);
+		ownerRepository.delete(id);
+//		if(true) throw new RuntimeException("testing rollback");
+	}
+
+}
